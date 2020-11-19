44f5a16c57622accc47f84aefe3f5ee437dde0a1
hamidadribika <hamida.dr02@yahoo.com>
2020-11-19 16:12:57 +0200

    updated services

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import Paint from "../paint.svg";
+import w from "../www.svg";
+import Youtube from "../youtube.svg";
 const Services = () => {
 	return (
 		// <section className="w-11/12 mx-auto h-screen bg-service bg-center bg-cover bg-fixed">
@@ -8,16 +10,21 @@ const Services = () => {
 		<div className="container">
 			<div className="services-left">
 					<h1>Services</h1>
-					<p>This is an introduction about our services</p>
+					<p>This is an introduction about our services. 
+						It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+					</p>
+				
 
 			</div>
 			<div className="services-right">
 				<div className="first-column">
-					<div className='service'>
+						<div className='service'>
+							<img src={w} alt="icon" className="ServiceIcon"/>
 						<h2>Web Design</h2>
 						<article>Lorem Ipsum is simply dummy text of the printing and typesetting industry.  </article>
 					</div>
-					<div className='service'>
+						<div className='service'>
+							<img src={Paint} alt="icon" className="ServiceIcon"/>
 					<h2>Social Media </h2>
 				<article>Lorem Ipsum is simply dummy text of the printing and typesetting industry.  </article>
 					</div>
@@ -25,11 +32,13 @@ const Services = () => {
 				</div>
 
 				<div className="second-column">
-					<div className='service'>
+						<div className='service'>
+							<img src={Paint} alt="icon" className="ServiceIcon"/>
 					<h2>Identity Branding </h2>
 						<article>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </article>
 					</div>
-					<div className='service'>
+						<div className='service'>
+							<img src={Youtube} alt="icon" className="ServiceIcon"/>
 					<h2>Logo Design</h2>
 						<article>Lorem Ipsum is simply dummy text of the printing and typesetting industry.  </article>
 					</div>
